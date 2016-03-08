6ef8baa7d293c32918d99cf25d4b6c3d37648fd7
Michael Sheely <msheely@cs.hmc.edu>
2016-03-07 16:11:25 -0800

    Removed executable from version control

---
+++ heapTest.cc
@@ -0,0 +1,11 @@
+#include "binHeap.h"
+#include <iostream>
+#include <math.h>
+using std::cout; using std::endl;
+
+int main() {
+  binHeap<float> floatHeap;
+  float x = floatHeap.findMin();
+  cout << "Min was " << x << "." << endl;
+  return 0;
+}
